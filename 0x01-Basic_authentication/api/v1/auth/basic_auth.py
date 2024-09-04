5a092df5ae4dfebcb8be0335f660d6796ef164ea
St <aaronndas@gmail.com>
2024-09-05 00:16:29 +0200

    niner

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ the basic auth class
 
 from .auth import Auth
 import base64
+from typing import TypeVar
 
 class BasicAuth(Auth):
     """
@@ -48,3 +49,29 @@ class BasicAuth(Auth):
         username, password = decoded_base64_authorization_header.split(":")
         return username, password
     
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        get user object from auth
+        """
+        if user_email is None or user_pwd is None:
+            return None
+        if type(user_email) == type(user_pwd) and type(user_pwd) != type("ji"):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        if len(users) <= 0:
+            return None
+        if users[0].is_valid_password(user_pwd):
+            return users[0]
+    
+def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the user from a request.
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
