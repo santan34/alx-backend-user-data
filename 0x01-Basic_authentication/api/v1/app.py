493f06ad7579ab835216e97fa498efb8dd60ae04
St <aaronndas@gmail.com>
2024-09-04 15:29:48 +0200

    auth shii

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,16 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = os.getenv('AUTH_TYPE', 'auth')
+if auth:
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -30,9 +35,22 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """ Forbidden handler
     """
-    return jsonify({"error": "Forbidden"}), 403
-
+    return jsonify({"error": "Forbidden"}), 
     
+@app.before_request
+def authenticate():
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is None:
+        return
+    if  not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+         abort(401)
+    if auth.current_user(request) is None:
+        return abort(403)
+
+
+
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -31,6 +31,8 @@ class Auth:
         """
         authorisation header
         """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> None:
