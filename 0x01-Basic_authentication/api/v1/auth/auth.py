836546cc029674875b7a666a2090474fdb34f27a
St <aaronndas@gmail.com>
2024-09-04 03:26:18 +0200

    mukuru

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ the auth class
 """
 from flask import request
 from typing import List
+import fnmatch
 
 
 class Auth:
@@ -15,7 +16,16 @@ class Auth:
         """
         require something
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded in excluded_paths:
+            if fnmatch.fnmatch(
+                    path, excluded) or fnmatch.fnmatch(
+                    f'{path}/', excluded):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> None:
         """
@@ -32,6 +42,11 @@ class Auth:
 
 if __name__ == "__main__":
     a = Auth()
+    print(a.require_auth(None, None))
+    print(a.require_auth(None, []))
+    print(a.require_auth("/api/v1/status/", []))
     print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-    print(a.authorization_header())
-    print(a.current_user())
+    print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
+    print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
+    print(a.require_auth("/api/v1/users",
+          ["/api/v1/status/", "/api/v1/stats"]))
