c0f876e9d79df5d4a05bfa9580c2bf31d94e0266
St <aaronndas@gmail.com>
2024-09-04 23:19:12 +0200

    eight

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ the basic auth class
 """
 
 from .auth import Auth
+import base64
 
 class BasicAuth(Auth):
     """
@@ -21,17 +22,17 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.replace('Basic ', '')
-
-if __name__ == "__main__":
-    a = BasicAuth()
-    print(a.extract_base64_authorization_header(None))
-    print(a.extract_base64_authorization_header(89))
-    print(a.extract_base64_authorization_header("Holberton School"))
-    print(a.extract_base64_authorization_header("Basic Holberton"))
-    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
-    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
-    print(a.extract_base64_authorization_header("Basic1234"))
-
-
-    
-    
+        
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        decode a base6 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != type("abab"):
+            return None
+        try:
+            res = base64.b64decode(base64_authorization_header)
+            return res.decode('utf-8')
+        except:
+            return None
