39bb9c2d51f9bab03787e5873cc97e91bc3fa280
St <aaronndas@gmail.com>
2024-09-12 17:25:10 +0200

    fixed autopep on zero

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 """create a model in sqlalchemy"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 base = declarative_base()
 
+
 class User(base):
     """
     creates a table tablename 'users'
     """
-    
+
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250))
-    hashed_password  = Column(String(250), nullable = False)
-    session_id = Column(String(250), nullable = True)
-    reset_token =  Column(String(250), nullable = True)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
